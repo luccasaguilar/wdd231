43ced58452632ffed1ab7d8326e88796e91fa227
Luccas Aguilar <luccasaguilar@gmail.com>
2025-10-03 11:28:44 -0400

    images and css

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -47,8 +47,8 @@ async function loadPlaces() {
       img.src = p.image;                          
       img.alt = p.alt || p.name;
       img.loading = 'lazy';
-      img.width = 376; 
-      img.height = 212;
+      img.width = 560; 
+      img.height = 300;
       fig.appendChild(img);
 
       const adr = document.createElement('address');
